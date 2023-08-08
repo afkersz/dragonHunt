39d9129757c711eebd671df2139c4bd47168fde5
brianchewy <brian.chew@sureapp.com>
2023-08-08 13:01:26 -0400

    changing for new release

--- cypress/fixtures/expectedDescriptions.js
+++ cypress/fixtures/expectedDescriptions.js
@@ -1,11 +1,11 @@
 export const expectedDescriptions = [
     "Mana flows like a current through this glassy egg.",
-    "It’s almost like time is distorted around this egg.",
+    //"It’s almost like time is distorted around this egg.",
     "This egg is very reflective, almost metallic-looking.",
     "This egg gives off a beautiful glow.",
-    "This egg smells faintly like brine.",
-    "Something about this egg seems to lure you in.",
-    "This shiny egg smells faintly like flowers.",
+    //"This egg smells faintly like brine.",
+    // "Something about this egg seems to lure you in.",
+    //"This shiny egg smells faintly like flowers.",
     // "This egg has an orange aura radiating from it.",
     "Delicate petals envelop this egg.",
 ];
