54e8e6845756def783825fcc3b7f723c16095b97
afkersz <81895060+afkersz@users.noreply.github.com>
2025-02-11 20:42:46 -0500

    reducing navigation to simple (#10)
    * reducing navigation to simple
    
    * change config
    
    * revert to runner
    
    * update to new runner 3
    
    * adding machine true
    
    * required syntax?
    
    * machine true
    
    * remove machine
    
    * minor formatting
    
    * add new file

--- cypress/e2e/dragoncave_navigation.spec.ts
+++ cypress/e2e/dragoncave_navigation.spec.ts
@@ -1,18 +1,15 @@
-import { checkTimeAndLogin } from "../support/utils/dragoncave/login_flow/login_util"
-import { eggSearchInEachBiome } from "../support/utils/dragoncave/egg_flow/egg_util"
+import { fillOutLoginAndPassword } from "../support/utils/dragoncave/login_flow/login_util"
 
 
-describe('User wants to be able to navigate and login into Dragon Cave successfully', {
+describe('Given User wants to be able to navigate and able to find specific eggs into Dragon Cave', {
 }, () => {
-  context('Given that we know our credentials for DragonCave', () => {
-    beforeEach('Navigates to DragonCave and Login', () => {
-      checkTimeAndLogin();
-    });
-
-    it('should navigate to different biomes, verify the specific description, and navigate to the next biome if it fails', () => {
-      eggSearchInEachBiome();
+  context('When that we want to verify our credentials for DragonCave', () => {
+    it('Then user navigates to DragonCave and Logs in', () => {
+      fillOutLoginAndPassword();
     });
   });
+  
 });
+
 
 
