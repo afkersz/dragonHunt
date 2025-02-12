b8e6b193b7d47abffcd96aec3498d9ec67c33536
afkersz <clarkvek@gmail.com>
2025-02-11 20:41:00 -0500

    minor formatting

--- cypress/support/utils/dragoncave/egg_flow/egg_util.ts
+++ cypress/support/utils/dragoncave/egg_flow/egg_util.ts
@@ -5,7 +5,7 @@ import { navigateBiome } from '../../../pages/biomePage.ts';
 
 export function eggSearchInEachBiome() {
 
-    let descriptionFound = false;
+    const descriptionFound = false;
 
     cy.visit('https://dragcave.net/dragons/1,1,0');
 
@@ -26,52 +26,37 @@ export function eggSearchInEachBiome() {
         })
         .then(() => {
             cy.log(`Number of "Egg" occurrences in the 3rd column: ${eggCount}`);
-            let expectedDescriptionsToUse = eggCount >= 2 ? expectedRareDescriptions : expectedDescriptions;
-
-
+            const expectedDescriptionsToUse = eggCount >= 2 ? expectedRareDescriptions : expectedDescriptions;
             cy.log(`Using ${expectedDescriptionsToUse === expectedRareDescriptions ? 'expectedRareDescriptions' : 'expectedDescriptions'}`);
-
-            //eggsearch
             cy.wrap(biomes).each((biome) => {
-                navigateBiome[biome](); // Navigate to the current biome
-                let biomeDescriptionFound = false; // Initialize a flag for the current biome
+                navigateBiome[biome]();
+                let biomeDescriptionFound = false;
 
                 cy.get('.eggs span[aria-hidden="true"]').each(($span) => {
                     const currentDescription = $span.text().trim();
 
                     expectedDescriptionsToUse.forEach((expectedDescription) => {
                         if (currentDescription === expectedDescription) {
-                            // The specific description is found, perform necessary assertions or actions
                             cy.wrap($span)
                                 .should('be.visible')
                                 .siblings('a')
                                 .click();
-
-                            // Perform any additional actions or assertions for the specific description
                             cy.log(`Found expected description: ${expectedDescription}`);
-
-                            // Set the flag to true for the current biome
                             biomeDescriptionFound = true;
                         }
                     });
                 }).then(() => {
                     if (!biomeDescriptionFound) {
-                        // The specific description is not found in the current biome
                         cy.log(`The specific description was not found in the ${biome} biome.`);
-                        // Perform any necessary assertions or actions specific to the current biome
                     }
                 });
             }).then(() => {
-                // After going through all the biomes, perform assertions for both scenarios
                 cy.wrap(biomes).each((biome) => {
-                    const foundInBiome = expectedDescriptionsToUse[biome] || false; // Get the flag value for each biome
-
+                    const foundInBiome = expectedDescriptionsToUse[biome] || false; 
                     if (foundInBiome) {
-                        // The specific description was found in the biome
-                        expect(foundInBiome).to.be.true; // Assertion for finding the expected description in the biome
+                        expect(foundInBiome).to.be.true; 
                     } else {
-                        // The specific description was not found in the biome
-                        expect(foundInBiome).to.be.false; // Assertion for not finding the expected description in the biome
+                        expect(foundInBiome).to.be.false; 
                     }
                 });
             });
