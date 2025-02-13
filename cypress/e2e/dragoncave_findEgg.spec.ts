b28c08a95ccc4ad4d3e7d9e846b3db13a76b84f6
afkersz <clarkvek@gmail.com>
2025-02-13 18:05:19 -0500

    remove test steps to just login and find eggs

--- cypress/e2e/dragoncave_findEgg.spec.ts
+++ cypress/e2e/dragoncave_findEgg.spec.ts
@@ -5,16 +5,12 @@ import { executeEggSearch} from "../support/utils/dragoncave/egg_flow/egg_util"
 describe('Given user wants successfully find specific eggs in each biome', {
 }, () => {
   context('When user has the correct credentials for DragonCave', () => {
-    before('Then user navigates to DragonCave to lgoin', () => {
+    before('User navigates to DragonCave to login', () => {
       //checkTimeAndLogin();
       fillOutLoginAndPassword();
     });
 
-    it('And user navigates to dragon page', () => {
-      cy.visit('/dragons/1,1,0');
-    });
-
-    it('And should verify that there are avaliable egg slots', () => {
+    it('Then search for specific eggs in each biome', () => {
       executeEggSearch();
     });
 
